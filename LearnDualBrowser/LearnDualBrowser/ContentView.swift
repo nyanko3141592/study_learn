9f7bbb5ba18cf800fabebc01ca8d6d694e4a3368
NanokiTakahashi <takahashinaoki521@gmail.com>
2019-10-03 19:32:30 +0900

    no message

---
+++ LearnDualBrowser/LearnDualBrowser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnDualBrowser
+//
+//  Created by TAKAHASHI Naoki on 2019/10/03.
+//  Copyright © 2019 TAKAHASHI Naoki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
